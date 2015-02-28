40e8b700beb1228029851af1cd64dde64db0eef6
alyphen <alyphen@gmail.com>
2015-02-28 10:25:29 +0000

    Initial commit

---
+++ desktop/src/io/github/alyphen/beatskaters/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.alyphen.beatskaters.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.alyphen.beatskaters.BeatSkaters;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BeatSkaters(), config);
+	}
+}
