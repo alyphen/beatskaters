123d43f1d684624cf1890147cd4c0b6a2c1fb0ee
alyphen <alyphen@gmail.com>
2015-02-28 16:03:19 +0000

    Add ducking mechanics

--- core/src/io/github/alyphen/beatskaters/GameScreen.java
+++ core/src/io/github/alyphen/beatskaters/GameScreen.java
@@ -8,13 +8,11 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.Array;
 
-import static com.badlogic.gdx.Input.Keys.UP;
-import static com.badlogic.gdx.Input.Keys.W;
+import static com.badlogic.gdx.Input.Keys.*;
 import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 import static com.badlogic.gdx.physics.box2d.BodyDef.BodyType.DynamicBody;
 import static com.badlogic.gdx.physics.box2d.BodyDef.BodyType.StaticBody;
-import static java.lang.Math.PI;
-import static java.lang.Math.min;
+import static java.lang.Math.*;
 
 public class GameScreen extends ScreenAdapter {
 
@@ -30,7 +28,6 @@ public class GameScreen extends ScreenAdapter {
     private Box2DDebugRenderer debugRenderer;
     private float accumulator;
     private String level;
-    
     private Body player;
 
     public GameScreen() {
@@ -47,6 +44,36 @@ public class GameScreen extends ScreenAdapter {
             public boolean keyDown(int keycode) {
                 if (keycode == UP || keycode == W) {
                     player.applyLinearImpulse(0, 15000, player.getPosition().x, player.getPosition().y, true);
+                } else if (keycode == DOWN || keycode == S) {
+                    player.destroyFixture(player.getFixtureList().get(0));
+                    PolygonShape duckingBox = new PolygonShape();
+                    duckingBox.setAsBox(32, 24);
+                    FixtureDef duckingFixtureDef = new FixtureDef();
+                    duckingFixtureDef.shape = duckingBox;
+                    duckingFixtureDef.density = 0.01F;
+                    duckingFixtureDef.friction = 0.1F;
+                    duckingFixtureDef.restitution = 0.1F;
+                    player.createFixture(duckingFixtureDef);
+                    duckingBox.dispose();
+                    player.setTransform(player.getPosition().x - (float) (24F * sin(player.getAngle())), player.getPosition().y - (float) (24F * cos(player.getAngle())), player.getAngle());
+                }
+                return true;
+            }
+
+            @Override
+            public boolean keyUp(int keycode) {
+                if (keycode == DOWN || keycode == S) {
+                    player.destroyFixture(player.getFixtureList().get(0));
+                    PolygonShape playerBox = new PolygonShape();
+                    playerBox.setAsBox(32, 48); // half-width and half-height again
+                    FixtureDef playerFixtureDef = new FixtureDef();
+                    playerFixtureDef.shape = playerBox;
+                    playerFixtureDef.density = 0.01F;
+                    playerFixtureDef.friction = 0.1F;
+                    playerFixtureDef.restitution = 0.1F;
+                    player.createFixture(playerFixtureDef);
+                    playerBox.dispose();
+                    player.setTransform(player.getPosition().x + (float) (24F * sin(player.getAngle())), player.getPosition().y + (float) (24F * cos(player.getAngle())), player.getAngle());
                 }
                 return true;
             }
@@ -101,13 +128,13 @@ public class GameScreen extends ScreenAdapter {
         playerDef.position.set(64, 256);
         player = world.createBody(playerDef);
         PolygonShape playerBox = new PolygonShape();
-        playerBox.setAsBox(32, 48); // half-width and half height again
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = playerBox;
-        fixtureDef.density = 0.01F;
-        fixtureDef.friction = 0.1F;
-        fixtureDef.restitution = 0.1F;
-        player.createFixture(fixtureDef);
+        playerBox.setAsBox(32, 48); // half-width and half-height again
+        FixtureDef playerFixtureDef = new FixtureDef();
+        playerFixtureDef.shape = playerBox;
+        playerFixtureDef.density = 0.01F;
+        playerFixtureDef.friction = 0.1F;
+        playerFixtureDef.restitution = 0.1F;
+        player.createFixture(playerFixtureDef);
         player.setGravityScale(50F);
         playerBox.dispose();
     }
