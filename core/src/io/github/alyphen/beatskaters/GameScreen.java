25b32d081a4acb2ca3d9f651e3f2acdba6f018d1
alyphen <alyphen@gmail.com>
2015-02-28 10:59:01 +0000

    Initial window dimensions and screen

---
+++ core/src/io/github/alyphen/beatskaters/GameScreen.java
@@ -0,0 +1,11 @@
+package io.github.alyphen.beatskaters;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+
+    @Override
+    public void render(float delta) {
+    }
+    
+}
